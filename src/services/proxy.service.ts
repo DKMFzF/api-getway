50628a1b88c334c5070e6921525733c6faa20f13
DKMFzF <150276575+DKMFzF@users.noreply.github.com>
2025-02-26 15:13:15 +0700

    add proxy.service

--- src/services/proxy.service.ts
+++ src/services/proxy.service.ts
@@ -1,15 +1,15 @@
-// import axios from 'axios';
+import axios from 'axios';
 
-// /**
-//  * функция для проксирования запросов к микросервисам
-//  * можно использовать эту функцию для любого запросак микросервисам
-//  */
+/**
+ * функция для проксирования запросов к микросервисам
+ * можно использовать эту функцию для любого запросак микросервисам
+ */
 
-// export const proxyRequest = async (url: string, method: 'GET' | 'POST', data?: any, headers?: any) => {
-//     try {
-//         const response = await axios({ url, method, data, headers });
-//         return response.data;
-//     } catch (error: any) {
-//         throw { status: error.response?.status || 500, message: error.message };
-//     }
-// };
+export const proxyRequest = async (url: string, method: 'GET' | 'POST', data?: any, headers?: any) => {
+    try {
+        const response = await axios({ url, method, data, headers });
+        return response.data;
+    } catch (error: any) {
+        throw { status: error.response?.status || 500, message: error.message };
+    }
+};
