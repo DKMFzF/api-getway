062d64aa1e1cb625e7ffbf74f0af8364f6088f1b
DKMFzF <150276575+DKMFzF@users.noreply.github.com>
2025-02-26 15:13:43 +0700

    add protected.controller

--- src/controllers/protected.controller.ts
+++ src/controllers/protected.controller.ts
@@ -1,19 +1,19 @@
-// import { Request, Response } from 'express';
-// import { proxyRequest } from '../services/proxy.service';
-// import { config } from '../config';
+import { Request, Response } from 'express';
+import { proxyRequest } from '../services/proxy.service';
+import { config } from '../config';
 
-// /** 
-//  * контроллер для защищенного маршрута 
-//  */
+/** 
+ * контроллер для защищенного маршрута 
+ */
 
-// export const protectedRoute = async (req: Request, res: Response) => {
-//     try {
-//         const token = req.headers.authorization;
-//         if (!token) return res.status(401).json({ error: '[ERROR]: Missing token' });
+export const protectedRoute = async (req: Request, res: Response) => {
+    try {
+        const token = req.headers.authorization;
+        if (!token) return res.status(401).json({ error: '[ERROR]: Missing token' });
 
-//         const data = await proxyRequest(`${config.AUTH_SERVICE_URL}/protected`, 'GET', null, { Authorization: token });
-//         res.json(data);
-//     } catch (error: any) {
-//         res.status(error.status).json({ error: error.message });
-//     }
-// };
+        const data = await proxyRequest(`${config.AUTH_SERVICE_URL}/protected`, 'GET', null, { Authorization: token });
+        res.json(data);
+    } catch (error: any) {
+        res.status(error.status).json({ error: error.message });
+    }
+};
